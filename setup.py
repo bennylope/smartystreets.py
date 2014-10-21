9afbcc133dd164738ba7e54e038127f8359737bb
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-10-21 15:44:21 -0400

    Lets call this alpha

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     zip_safe=False,
     keywords='smartystreets',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
