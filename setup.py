e272db44bebf55cd4d53d74f6cbc04adce040179
Ben Lopatin <ben@benlopatin.com>
2021-09-24 13:58:02 -0400

    Move package code to src

--- setup.py
+++ setup.py
@@ -1,68 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+See setup.cfg for packaging settings
+"""
 
-import os
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-
-with open("smartystreets/__init__.py", "r") as module_file:
-    for line in module_file:
-        if line.startswith("__version__"):
-            version_string = line.split("=")[1]
-            version = version_string.strip().replace("'", "")
-
-
-requirements = [
-    'requests>=2.0.0',
-]
-
-test_requirements = [
-    'mock>=1.0.0',
-    'responses==0.3.0',
-]
-
-
-setup(
-    name='smartystreets.py',
-    version=version,
-    description='A wrapper for the SmartyStreets address validation and geolocation API"',
-    long_description=readme + '\n\n' + history,
-    author='Ben Lopatin',
-    author_email='ben@wellfire.co',
-    url='https://github.com/bennylope/smartystreets.py',
-    packages=[
-        'smartystreets',
-    ],
-    package_dir={'smartystreets':
-                 'smartystreets'},
-    include_package_data=True,
-    install_requires=requirements,
-    license="BSD",
-    zip_safe=False,
-    keywords='smarty streets',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements,
-)
+from setuptools import setup
+setup()
