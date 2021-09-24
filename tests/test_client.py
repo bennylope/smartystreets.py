52ed96fc8896e3d1a83d0b663974e3e0e755fedc
Ben Lopatin <ben@benlopatin.com>
2021-09-24 15:37:32 -0400

    Switch tests to pytest

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,122 +8,123 @@ test_smartystreets
 Tests for `smartystreets` module.
 """
 
+import pytest
 import responses
-import unittest
-from unittest.mock import MagicMock
 
 from smartystreets.client import Client, validate_args, truncate_args, stringify
 from smartystreets.data import Address, AddressCollection
 from smartystreets.exceptions import (SmartyStreetsInputError, SmartyStreetsAuthError,
                                       SmartyStreetsPaymentError, SmartyStreetsServerError)
 
+@pytest.fixture
+def smarty_client():
+    yield Client(auth_id='blah', auth_token='blibbidy')
 
-class TestDecorators(unittest.TestCase):
+
+class TestDecorators:
     """
     The decorator functions provide some safety around the parameters provided to API calls
     """
 
-    def test_truncate_args(self):
+    def test_truncate_args(self, mocker):
 
-        selfarg = MagicMock()
+        selfarg = mocker.MagicMock()
         myargs = [i for i in range(104)]
 
         def somefunc(myself, args):
             return len(args)
 
         selfarg.truncate_addresses = True
-        self.assertEqual(truncate_args(somefunc)(selfarg, myargs), 100)
+        assert truncate_args(somefunc)(selfarg, myargs) == 100
 
         selfarg.truncate_addresses = False
         modified_func = truncate_args(somefunc)
-        self.assertRaises(ValueError, modified_func, selfarg, myargs)
+        with pytest.raises(ValueError):
+            modified_func(selfarg, myargs)
 
-    def test_validate_args(self):
+    def test_validate_args(self, mocker):
 
         def somefunc(myself, args):
             return len(args)
 
-        selfarg = MagicMock()
+        selfarg = mocker.MagicMock()
 
         myargs = ["1", "2", "3"]
-        self.assertEqual(validate_args(somefunc)(selfarg, myargs), 3)
+        assert validate_args(somefunc)(selfarg, myargs) == 3
 
         myargs = [1, 2, 3]
         modified_func = validate_args(somefunc)
-        self.assertRaises(TypeError, modified_func, selfarg, myargs)
+        with pytest.raises(TypeError):
+            modified_func(selfarg, myargs)
 
         myargs = [{}, "?"]
         modified_func = validate_args(somefunc)
-        self.assertRaises(TypeError, modified_func, selfarg, myargs)
+        with pytest.raises(TypeError):
+            modified_func(selfarg, myargs)
 
 
-class TestClient(unittest.TestCase):
-
-    def setUp(self):
-        self.client = Client(auth_id='blah', auth_token='blibbidy')
+class TestClient:
 
     @responses.activate
-    def test_input_error(self):
+    def test_input_error(self, smarty_client):
         responses.add(responses.POST, 'https://api.smartystreets.com/street-address',
                   body='', status=400,
                   content_type='application/json')
-        self.assertRaises(SmartyStreetsInputError, self.client.street_addresses, [{}, {}])
+        with pytest.raises(SmartyStreetsInputError):
+            smarty_client.street_addresses([{}, {}])
 
     @responses.activate
-    def test_auth_error(self):
+    def test_auth_error(self, smarty_client):
         responses.add(responses.POST, 'https://api.smartystreets.com/street-address',
                       body='', status=401,
                       content_type='application/json')
-        self.assertRaises(SmartyStreetsAuthError, self.client.street_addresses, [{}, {}])
+        with pytest.raises(SmartyStreetsAuthError):
+            smarty_client.street_addresses([{}, {}])
 
     @responses.activate
-    def test_payment_error(self):
+    def test_payment_error(self, smarty_client):
         responses.add(responses.POST, 'https://api.smartystreets.com/street-address',
                       body='', status=402,
                       content_type='application/json')
-        self.assertRaises(SmartyStreetsPaymentError, self.client.street_addresses, [{}, {}])
+        with pytest.raises(SmartyStreetsPaymentError):
+            smarty_client.street_addresses([{}, {}])
 
     @responses.activate
-    def test_server_error(self):
+    def test_server_error(self, smarty_client):
         responses.add(responses.POST, 'https://api.smartystreets.com/street-address',
                       body='', status=500,
                       content_type='application/json')
-        self.assertRaises(SmartyStreetsServerError, self.client.street_addresses, [{}, {}])
+        with pytest.raises(SmartyStreetsServerError):
+            smarty_client.street_addresses([{}, {}])
 
     @responses.activate
-    def test_one_address(self):
+    def test_one_address(self, smarty_client):
         """Ensure singluar street address method returns an Address"""
         responses.add(responses.POST, 'https://api.smartystreets.com/street-address',
                       body='[{"street_address": "100 Main St"}]',
                       status=200, content_type='application/json')
-        response = self.client.street_address({"street": "100 Main st"})
-        self.assertIsInstance(response, Address)
+        response = smarty_client.street_address({"street": "100 Main st"})
+        assert isinstance(response, Address)
 
     @responses.activate
-    def test_addresses_response(self):
+    def test_addresses_response(self, smarty_client):
         """Ensure address return an AddressCollection"""
         responses.add(responses.POST, 'https://api.smartystreets.com/street-address',
                       body='[{"street_address": "100 Main St"}, {"street_address": "200 Main St"}]',
                       status=200, content_type='application/json')
-        response = self.client.street_addresses([{"street": "100 Main st"},
+        response = smarty_client.street_addresses([{"street": "100 Main st"},
                                                  {"street": "200 Main St"}])
-        self.assertIsInstance(response, AddressCollection)
-        self.assertEqual(2, len(response))
+        assert isinstance(response, AddressCollection)
+        assert len(response) == 2
 
 
-class TestDataValidation(unittest.TestCase):
+class TestDataValidation:
     """
     SmartyStreets expects JSON values as strings - test that all data is updated that way
     """
 
     def test_stringify(self):
-        self.assertEqual(
-            stringify([{"input_id": 123, "zipcode": 20120, "candidates": "9"}]),
-            [{"input_id": "123", "zipcode": "20120", "candidates": 9}],
-        )
+        assert stringify([{"input_id": 123, "zipcode": 20120, "candidates": "9"}]) == [{"input_id": "123", "zipcode": "20120", "candidates": 9}]
 
     def test_padded_integer(self):
-        self.assertEqual(
-            stringify([{"input_id": 123, "zipcode": 120, "candidates": "9"}]),
-            [{"input_id": "123", "zipcode": "00120", "candidates": 9}],
-        )
+        assert  stringify([{"input_id": 123, "zipcode": 120, "candidates": "9"}]) == [{"input_id": "123", "zipcode": "00120", "candidates": 9}]
--- tests/test_data.py
+++ tests/test_data.py
@@ -8,31 +8,30 @@ test_smartystreets
 Tests for `smartystreets` module.
 """
 
-import unittest
-
+import pytest
 from smartystreets.data import Address, AddressCollection
 
 
-class TestAddress(unittest.TestCase):
+class TestAddress:
 
     def test_empty(self):
         addr = Address([])
-        self.assertIsNone(addr.location)
+        assert addr.location is None
 
     def test_location(self):
         addr = Address({'metadata': {'latitude': 12, 'longitude': 13}})
-        self.assertEqual(addr.location, (12, 13))
+        assert addr.location == (12, 13)
 
     def test_id(self):
         addr = Address({'input_id': 'test1'})
-        self.assertEqual(addr.id, 'test1')
+        assert addr.id == 'test1'
 
     def test_index(self):
         addr = Address({'input_id': 'test1', 'input_index': 4})
-        self.assertEqual(addr.index, 4)
+        assert addr.index == 4
 
 
-class TestCollection(unittest.TestCase):
+class TestAddressCollection:
 
     def test_get_by_id(self):
         """Ensure an address can be found by input_id"""
@@ -41,11 +40,12 @@ class TestCollection(unittest.TestCase):
             {'input_index': 5, 'input_id': 'h'},
             {'input_index': 8, 'input_id': 'X'},
         ])
-        self.assertEqual(collection.get('A').index, 3)
-        self.assertEqual(collection.get('h').index, 5)
-        self.assertEqual(collection.get('X').index, 8)
+        assert collection.get('A').index == 3
+        assert collection.get('h').index == 5
+        assert collection.get('X').index == 8
 
-        self.assertRaises(KeyError, collection.get_index, 'kj')
+        with pytest.raises(KeyError):
+            collection.get_index('kj')
 
     def test_get_by_index(self):
         """Ensure an address can be found by input index"""
@@ -54,11 +54,12 @@ class TestCollection(unittest.TestCase):
             {'input_index': 5, 'input_id': 'h'},
             {'input_index': 8, 'input_id': 'X'},
         ])
-        self.assertEqual(collection.get_index(3).id, 'A')
-        self.assertEqual(collection.get_index(5).id, 'h')
-        self.assertEqual(collection.get_index(8).id, 'X')
+        assert collection.get_index(3).id == 'A'
+        assert collection.get_index(5).id == 'h'
+        assert collection.get_index(8).id == 'X'
 
-        self.assertRaises(KeyError, collection.get_index, 10)
+        with pytest.raises(KeyError):
+            collection.get_index(10)
 
     def test_get_by_list_index(self):
         """Normal list indexing is unaffected"""
@@ -67,10 +68,8 @@ class TestCollection(unittest.TestCase):
             {'input_index': 1, 'input_id': 'h'},
             {'input_index': 2, 'input_id': 'X'},
         ])
-        self.assertEqual(collection[0].id, 'A')
-        self.assertEqual(collection[1].id, 'h')
-        self.assertEqual(collection[2].id, 'X')
+        assert collection[0].id ==  'A'
+        assert collection[1].id == 'h'
+        assert collection[2].id == 'X'
 
 
-if __name__ == '__main__':
-    unittest.main()
