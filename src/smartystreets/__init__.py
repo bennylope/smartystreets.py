a13fa7c0efd7bf93fabfa0cf93436ffa571b414a
Ben Lopatin <ben@benlopatin.com>
2021-12-29 16:50:30 -0500

    Add AsyncClient

--- src/smartystreets/__init__.py
+++ src/smartystreets/__init__.py
@@ -2,9 +2,10 @@
 
 __author__ = "Ben Lopatin"
 __email__ = "ben@benlopatin.com"
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 
 from smartystreets.client import Client  # noqa
+from smartystreets.async_client import AsyncClient  # noqa
 
-__all__ = ["Client"]
+__all__ = ["Client", "AsyncClient"]
